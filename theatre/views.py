af6c6d0dc89d2217c8b3455cce3665381287f9dd
Roman28101 <r.kovalenko28101@gmail.com>
2023-07-19 23:21:13 +0800

    Create viewsets for actor, genre, theatre_hall and play models

--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,14 @@
-from rest_framework import mixins
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from theatre.models import Actor, Genre, TheatreHall, Play
 from theatre.serializers import ActorSerializer, \
-    GenreSerializer, TheatreHallSerializer, PlaySerializer
+    GenreSerializer, TheatreHallSerializer, PlaySerializer, \
+    PlayListSerializer, PlayDetailSerializer, \
+    PlayPosterSerializer
 
 
 class ActorViewSet(
@@ -42,3 +47,53 @@ class PlayViewSet(
 ):
     queryset = Play.objects.prefetch_related("genres", "actors")
     serializer_class = PlaySerializer
+
+    @staticmethod
+    def params_to_int(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self.params_to_int(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self.params_to_int(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        if self.action == "reitrieve":
+            return PlayDetailSerializer
+
+        if self.action == "upload_poster":
+            return PlayPosterSerializer
+
+        return PlaySerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-poster",
+        permission_classes=[IsAdminUser]
+    )
+    def upload_poster(self, request, pk=None):
+        play = self.get_object()
+        serializer = self.get_serializer(play, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
