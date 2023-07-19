05f2d7e0be01f032beac07fe6d91b54122aceecc
Roman28101 <r.kovalenko28101@gmail.com>
2023-07-19 11:30:50 +0800

    Create app user with user model, user registration with email and password

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "theatre",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = "theatre_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
